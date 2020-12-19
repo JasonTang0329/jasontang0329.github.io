6b8104dbd387a9969bcd365c860351f096bb5e7c
Jason <33599669+JasonTang0329@users.noreply.github.com>
2020-12-19 23:14:41 +0800

    Update New version

--- web/src/management.js
+++ web/src/management.js
@@ -1,52 +1,61 @@
 var Main = {
-    data() {
-      return {
-        data: [{
-          label: '一级 1',
+  data() {
+    return {
+      treeData: [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
           children: [{
-            label: '二级 1-1',
-            children: [{
-              label: '三级 1-1-1'
-            }]
+            label: '三级 1-1-1'
+          }]
+        }]
+      }, {
+        label: '一级 2',
+        children: [{
+          label: '二级 2-1',
+          children: [{
+            label: '三级 2-1-1'
           }]
         }, {
-          label: '一级 2',
+          label: '二级 2-2',
           children: [{
-            label: '二级 2-1',
-            children: [{
-              label: '三级 2-1-1'
-            }]
-          }, {
-            label: '二级 2-2',
-            children: [{
-              label: '三级 2-2-1'
-            }]
+            label: '三级 2-2-1'
+          }]
+        }]
+      }, {
+        label: '一级 3',
+        children: [{
+          label: '二级 3-1',
+          children: [{
+            label: '三级 3-1-1'
           }]
         }, {
-          label: '一级 3',
+          label: '二级 3-2',
           children: [{
-            label: '二级 3-1',
-            children: [{
-              label: '三级 3-1-1'
-            }]
-          }, {
-            label: '二级 3-2',
-            children: [{
-              label: '三级 3-2-1'
-            }]
+            label: '三级 3-2-1'
           }]
-        }],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
-      };
-    },
-    methods: {
-      handleNodeClick(data) {
-        console.log(data);
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      },
+      dialogControl: {
+        userDialogVisible: false,
+        roleDialogVisible: false,
+        funcDialogVisible: false,
+        unitDialogVisible: false,
+        messageSetupDialogVisible: false,
+        codeDialogVisible: false,
+        syscodeDialogVisible: false,
       }
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
     }
-  };
+  }
+};
 var Ctor = Vue.extend(Main)
 new Ctor().$mount('#app')
